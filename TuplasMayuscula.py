7f9ebbe687e1301196767ea1d73bbfaeb954d7c1
Adriana <adriana.murillo@tecnologicoliceocristiano.edu.ec>
2024-08-01 18:17:49 -0500

    Iteracion ciclos

---
+++ TuplasMayuscula.py
@@ -0,0 +1,5 @@
+# Instrucciones:  Dada un lista de palabras ['python', 'es', 'genial'], usa un ciclo for para imprimir cada palabra en mayuscula.
+
+palabra = ['python', 'es', 'genial']
+palabra1 = [letra.upper() for letra in palabra]
+print(palabra1)
